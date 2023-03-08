46e2a45a252148a7b7cf0ab593f7af28f1f145e9
Nima <nimasaranga123@gmail.com>
2023-03-08 23:08:35 +0530

    Added customer details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';  
-  
+
 void main() {  
   runApp(MaterialApp(  
     title: 'Flutter Navigation',  
@@ -46,6 +46,12 @@ class FirstRoute extends StatelessWidget {
               padding: const EdgeInsets.all(5.0),
               child: Text("Electronic Store", style: TextStyle(fontSize: 30,),),
             ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Image.asset('assets/customer1.png', height: 10, width: 20,fit: BoxFit.fill),
+            ),
+                
+             
             SizedBox(
               height: 100, // <-- SEE HERE
             ),
@@ -73,7 +79,7 @@ class FirstRoute extends StatelessWidget {
               padding: const EdgeInsets.all(5.0),
               child: TextFormField(
                 decoration: const InputDecoration(
-                  icon: const Icon(Icons.person),
+                  icon: const Icon(Icons.password),
                   hintText: 'Password',
                   labelText: 'Password',
                   border: OutlineInputBorder(),
@@ -124,18 +130,6 @@ class FirstRoute extends StatelessWidget {
   }  
 }
 
-
-
-
-
-
-
-
-
-
-
- 
-  
 class SecondRoute extends StatelessWidget {  
   @override  
   Widget build(BuildContext context) {  
@@ -177,7 +171,7 @@ class SecondRoute extends StatelessWidget {
                 onPressed: () {
                    Navigator.push(  
               context,  
-              MaterialPageRoute(builder: (context) => thirdRoute()),  
+              MaterialPageRoute(builder: (context) => seventhRoute()),  
             );  
                   // It returns true if the form is valid, otherwise returns false
                   // if (_formKey.currentState!.validate()) {
@@ -201,7 +195,7 @@ class SecondRoute extends StatelessWidget {
                 onPressed: () {
                    Navigator.push(  
               context,  
-              MaterialPageRoute(builder: (context) => SecondRoute()),  
+              MaterialPageRoute(builder: (context) => sixthRoute()),  
             );  
                   // It returns true if the form is valid, otherwise returns false
                   // if (_formKey.currentState!.validate()) {
@@ -415,7 +409,7 @@ class fourthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(10.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.local_post_office),
                 hintText: 'Business name',
                 labelText: 'Business Name',
                 border: OutlineInputBorder(),
@@ -435,7 +429,7 @@ class fourthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.numbers),
                 hintText: 'GST number',
                 labelText: 'GST number',
                 border: OutlineInputBorder(),
@@ -475,7 +469,7 @@ class fourthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.call),
                 hintText: 'Business contact number',
                 labelText: 'Business contact number',
                 border: OutlineInputBorder(),
@@ -495,7 +489,7 @@ class fourthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.mobile_friendly),
                 hintText: 'Personal contact number',
                 labelText: 'Personal contact number',
                 border: OutlineInputBorder(),
@@ -515,7 +509,7 @@ class fourthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.email),
                 hintText: 'Email',
                 labelText: 'Email',
                 border: OutlineInputBorder(),
@@ -591,7 +585,7 @@ class fifthRoute extends StatelessWidget {
       ),
     
         child: Column(
-          
+        
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
                   // Container(
@@ -611,7 +605,7 @@ class fifthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(10.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.numbers),
                 hintText: 'Supplier id',
                 labelText: 'Supplier id',
                 border: OutlineInputBorder(),
@@ -631,7 +625,7 @@ class fifthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.add_box),
                 hintText: 'Item code',
                 labelText: 'Item code',
                 border: OutlineInputBorder(),
@@ -652,7 +646,7 @@ class fifthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.production_quantity_limits),
                 hintText: 'Quantity',
                 labelText: 'Quantity',
                 border: OutlineInputBorder(),
@@ -672,7 +666,7 @@ class fifthRoute extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
               decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
+                icon: const Icon(Icons.money),
                 hintText: 'Amount',
                 labelText: 'Amount',
                 border: OutlineInputBorder(),
@@ -723,6 +717,400 @@ class fifthRoute extends StatelessWidget {
     ));  
   }  
 }
+
+// Adding the item attributes
+
+//supplier
+class sixthRoute extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
+    return Container (
+      decoration:BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/bg.jpg'), fit: BoxFit.cover)
+      ),
+    
+      
+      child: Scaffold(  
+
+
+      appBar: AppBar(  
+        title: Text('Add items'),  
+        backgroundColor: Colors.purple,
+      ),  
+      body: Center(  
+         child: Container(
+          decoration:BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/bg.jpg'), fit: BoxFit.cover)
+      ),
+    
+        child: Column(
+          
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+                  // Container(
+                  //     child: Text("Login Page", style: TextStyle(fontSize: 20))),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 40, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text("Add the Item details", style: TextStyle(fontSize: 20,),),
+          ),
+          SizedBox(
+            height: 10, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.gif_box_sharp),
+                hintText: 'Item name',
+                labelText: 'Item Name',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.details),
+                hintText: 'Item specification',
+                labelText: 'Item specification',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.numbers_sharp),
+                hintText: 'HSN code',
+                labelText: 'HSN code',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.money),
+                hintText: 'Amount',
+                labelText: 'Amount',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.support_agent),
+                hintText: 'Supplier id',
+                labelText: 'Supplier id',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.image),
+                hintText: 'Item Images',
+                labelText: 'Item Images',
+                border: OutlineInputBorder(),
+              ),
+              
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+       
+        
+          // ignore: unnecessary_new
+          new Container(
+              padding: const EdgeInsets.only(left: 150.0, top: 20.0),
+              
+              child: new ElevatedButton(
+                 style: ElevatedButton.styleFrom(
+                  primary: Colors.purple, // background
+                  onPrimary: Colors.white, // foreground
+  ),
+                child: const Text('Add Items'),
+
+
+                onPressed: () {
+                   Navigator.push(  
+              context,  
+              MaterialPageRoute(builder: (context) => SecondRoute()),  
+            );  
+                  // It returns true if the form is valid, otherwise returns false
+                  // if (_formKey.currentState!.validate()) {
+                  //   // If the form is valid, display a Snackbar.
+                  // }
+                },
+              )),
+                  
+        ],
+      ),
+       
+      ),  
+    )
+    ));  
+  }  
+}
+
+class seventhRoute extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
+    return Container (
+      decoration:BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/bg.jpg'), fit: BoxFit.cover)
+      ),
+    
+      
+      child: Scaffold(  
+
+
+      appBar: AppBar(  
+        title: Text('Add B2B customer details'),  
+        backgroundColor: Colors.purple,
+      ),  
+      body: Center(  
+         child: Container(
+          decoration:BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/bg.jpg'), fit: BoxFit.cover)
+      ),
+    
+        child: Column(
+          
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+                  // Container(
+                  //     child: Text("Login Page", style: TextStyle(fontSize: 20))),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 40, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text("Add the customer details here(B2B)", style: TextStyle(fontSize: 20,),),
+          ),
+          SizedBox(
+            height: 10, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.local_post_office),
+                hintText: 'Business Name:',
+                labelText: 'Business Name:',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.numbers),
+                hintText: 'GST number',
+                labelText: 'GST number',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.person_2_rounded),
+                hintText: 'Authorised persons name',
+                labelText: 'Authorised persons name',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.phone),
+                hintText: 'Business contact number',
+                labelText: 'Business contact number::',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.mobile_friendly),
+                hintText: 'Personal contact number:',
+                labelText: 'Personal contact number:',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.email),
+                hintText: 'Email',
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+              
+            ),
+          ),
+          SizedBox(
+            height: 1, // <-- SEE HERE
+          ),
+       
+        
+          // ignore: unnecessary_new
+          new Container(
+              padding: const EdgeInsets.only(left: 150.0, top: 20.0),
+              
+              child: new ElevatedButton(
+                 style: ElevatedButton.styleFrom(
+                  primary: Colors.purple, // background
+                  onPrimary: Colors.white, // foreground
+  ),
+                child: const Text('Add Customers'),
+
+
+                onPressed: () {
+                   Navigator.push(  
+              context,  
+              MaterialPageRoute(builder: (context) => SecondRoute()),  
+            );  
+                  // It returns true if the form is valid, otherwise returns false
+                  // if (_formKey.currentState!.validate()) {
+                  //   // If the form is valid, display a Snackbar.
+                  // }
+                },
+              )),
+                  
+        ],
+      ),
+       
+      ),  
+    )
+    ));  
+  }  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 
 
