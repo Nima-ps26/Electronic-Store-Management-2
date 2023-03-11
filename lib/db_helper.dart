eca47f5f08c56bd41111dee55831703baecb0622
Nima <nimasaranga123@gmail.com>
2023-03-11 22:05:49 +0530

    Updated supplier

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -3,7 +3,7 @@ import 'package:sqflite/sqflite.dart' as sql;
 class SQLHelper{
   static Future<void>  createTables(sql.Database database) async {
     await database.execute("""CREATE TABLE data( 
-      id INTEGER PRIMARY KEY AUTOINNCREMENT NOT NULL,
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
       title TEXT,
       desc TEXT,
       createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -22,7 +22,7 @@ class SQLHelper{
     final db =  await SQLHelper.db();
 
     final data = {'title':title, 'desc': desc};
-    final id = await db.insert('data', data,conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    final id = await db.insert('data', data, conflictAlgorithm: sql.ConflictAlgorithm.replace);
     return id;
  
   }
